b9431c8e5d5aa2394a5a32fe3785cb5b62d25afa
lgautreaux <logangautreaux17@yahoo.com>
2024-05-02 10:33:17 -0700

    Update HomeController.java

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -1,5 +1,5 @@
 package com.example.demo.Controllers;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
  * HomeController
  */
 @RestController
-// @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHome() {
         return "Welcome to the home page";
     }
