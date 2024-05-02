a5c967d85b9b98e4424059bc1c6ef61a0d045224
lgautreaux <logangautreaux17@yahoo.com>
2024-05-02 10:36:45 -0700

    Update RestaurantController.java

--- src/main/java/com/example/demo/Controllers/RestaurantController.java
+++ src/main/java/com/example/demo/Controllers/RestaurantController.java
@@ -1,10 +1,35 @@
 package com.example.demo.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Models.Restaurant;
+import com.example.demo.Repositories.RestaurantRepository;
 
 @RestController
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
     
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @GetMapping("/{borough}")
+    public ResponseEntity<List<Restaurant>> getRestaurantByBorough(
+            @PathVariable(value = "borough") String boroughName) {
+            
+        List<Restaurant> result = restaurantRepository.findRestaurantByBorough(boroughName);
+
+        if (result != null) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
